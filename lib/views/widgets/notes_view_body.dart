dc7707aa9ef6ace71b317ea042e84797496990a8
khaledmju <khaledjoouu@gmail.com>
2026-05-10 13:23:27 +0300

    adding notes list view

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'note_item.dart';
+
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -11,8 +12,10 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children: [SizedBox(height: 50), CustomAppBar(), NoteItem(),
-
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
