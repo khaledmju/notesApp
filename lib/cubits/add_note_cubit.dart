cea83909fbd0e6ce5138c411ecd124d17ee90ef8
khaledmju <khaledjoouu@gmail.com>
2026-05-12 15:29:42 +0300

    adding add note cubit and multiBloc Provider

--- lib/cubits/add_note_cubit.dart
+++ lib/cubits/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notesapp/constans.dart';
+import 'package:notesapp/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  Future<void> addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/constans.dart';
+import 'package:notesapp/cubits/add_note_cubit.dart';
+import 'package:notesapp/models/note_model.dart';
 import 'package:notesapp/views/NotesView.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+
+  // this is will tell the hive that ill start  saving  data
+  Hive.registerAdapter(NoteModelAdapter());
 
   runApp(const NotesApp());
 }
@@ -15,20 +22,23 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //     // brightness: Brightness.dark // this is for background color or we use this
-      //   // scaffoldBackgroundColor: Color(0xff2E2D2F),
-      //
-      //
-      // ),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // theme: ThemeData(
+        //     // brightness: Brightness.dark // this is for background color or we use this
+        //   // scaffoldBackgroundColor: Color(0xff2E2D2F),
+        //
+        //
+        // ),
 
-      // or this
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color(0xff2E2D2F),
+        // or this
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color(0xff2E2D2F),
+        ),
+        home: NotesView(),
       ),
-      home: NotesView(),
     );
   }
 }
